b7957b3b63d5a15e8e95d8ff70e10a3e6510efa5
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2026-07-09 07:58:34 +0530

    redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import MainPage from './MainPage'
-import { Route, Routes } from 'react-router-dom'
-import Khat from './khat'
+import "./App.css";
+import MainPage from "./MainPage";
+import { Route, Routes } from "react-router-dom";
+import Khat from "./khat";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/khat" element={<Khat />} />
-   
-    </Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/khat" element={<Khat />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
