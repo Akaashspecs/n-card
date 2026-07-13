5d584b9e9730a261e3d0a6a6d43ec4cbc62a33e0
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2026-07-13 09:28:52 +0530

    rm

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,7 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="*" element={<Khat />} />
-      </Routes>
+      <Routes></Routes>
     </>
   );
 }
